6a9a32fcb8a1ba1f83998df9378b8c8a717a422e
mart <90089382+murumart@users.noreply.github.com>
2026-01-17 22:39:44 +0200

    reset branch

--- world/generation/block_layers/block_layer.gd
+++
@@ -1,4 +0,0 @@
-class_name BlockLayer extends Resource
-
-@export var block_type: int
-@export var distance := 0
--- world/generation/noises/noise_layer.gd
+++
@@ -1,7 +0,0 @@
-class_name NoiseLayersNoiseLayer extends Resource
-
-@export var noise: FastNoiseLite
-
-
-func get_density(pos: Vector3) -> float:
-	return noise.get_noise_3dv(pos)
